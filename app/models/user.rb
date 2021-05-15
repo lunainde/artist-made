a3622978575b58ddbd8465a906bde90f671fab27
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-15 15:23:22 +0200

    added shopping_cart initialization on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :shopping_carts
+  after_create :initialize_empty_shopping_cart
+
+  # not private, because we want to re-use it, when we set an old shopping cart to paid
+  def initialize_empty_shopping_cart
+    ShoppingCart.create!(total: 0, user: self)
+  end
 end
