e40d8382dbb2bd9c646f493936c4ee779ed1c987
ManjaBadel <m.badel@gmx.de>
2021-05-15 13:25:40 +0200

    index and show page for art_items

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :arts, only: [:index, :show]
+  resources :art_items, only: [:index, :show]
 end
