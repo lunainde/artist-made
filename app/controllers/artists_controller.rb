1a58ae7d555fc4eb545633fa22bb32b9409d53b7
ManjaBadel <m.badel@gmx.de>
2021-05-17 20:37:50 +0200

    fix routes and methods for artist index and show page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,13 +4,16 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
     @artists = policy_scope(Artist).order(created_at: :desc)
   end
+
   def show
-    @art = Art.find(params[:art_id])
+    @artist = Artist.find(params[:id])
+    authorize @artist
+    # @art = Art.find(params[:art_id])
   end
 
- def artist_params
-    params.require(:artist).permit(:first_name, :last_name, :art_type, :url_artists_photo)
-    authorize @artist
-  end
+ # def artist_params
+ #    params.require(:artist).permit(:first_name, :last_name, :art_type, :url_artists_photo)
+ #    authorize @artist
+ #  end
 end
 
