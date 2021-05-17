9aae06eb457c25fc28df0b6c2f25157b92e69d2d
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-17 20:43:29 +0200

    some cleaning

--- app/controllers/shopping_cart_items_controller.rb
+++ app/controllers/shopping_cart_items_controller.rb
@@ -1,5 +1,8 @@
 class ShoppingCartItemsController < ApplicationController
 
+  def update_or_create
+  end
+
   def create
     shopping_cart = current_user.shopping_carts.find_by(paid: false)
     @art_item = ArtItem.find(params[:art_item_id])
@@ -8,11 +11,8 @@ class ShoppingCartItemsController < ApplicationController
     @shopping_cart_item = ShoppingCartItem.new(art_item: @art_item, shopping_cart: shopping_cart, quantity: quantity, price: price)
     authorize @shopping_cart_item
     if @shopping_cart_item.save
-      # raise
       redirect_to art_path(@art_item.art),
       alert: "#{ @shopping_cart_item.quantity } x #{@shopping_cart_item.art_item.art.title} - #{ @shopping_cart_item.art_item.format } added to shopping cart!"
-      # @message = "Added #{ @shopping_cart_item.quantity } #{ @shopping_cart_item.art_item.art.title } to Shopping cart"
-      # render 'art_items/show.html.erb'
     else
       render 'art_items/show.html.erb'
     end
