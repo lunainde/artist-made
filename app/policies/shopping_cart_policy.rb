a7e569fa9a27fda23f2f7449294dcc3fa588c00c
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-21 20:35:10 +0200

    added paid shopping carts feature

--- app/policies/shopping_cart_policy.rb
+++ app/policies/shopping_cart_policy.rb
@@ -1,10 +1,11 @@
 class ShoppingCartPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
   end
   def show?
     record.user == user
   end
+
 end
