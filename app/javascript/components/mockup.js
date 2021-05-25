14ab9c2e3589db5fa564932bcd1c1c229871a810
ManjaBadel <m.badel@gmx.de>
2021-05-25 21:53:51 +0200

    update

--- app/javascript/components/mockup.js
+++ app/javascript/components/mockup.js
@@ -7,27 +7,51 @@ const changeToMockupPicture = () => {
   const adjustMockupByShape= document.getElementById('art-card-picture-shape')
 
   mockupPictures.forEach(picture => {
-    picture.addEventListener('click', () => {
+
+
+
+    picture.addEventListener('click', (e) => {
+
       changingPicture.style.backgroundImage = picture.style.backgroundImage;
 
+
+      // console.log(e.currentTarget)
+      // e.currentTarget.style.filter = 'opacity(1)';
+
+      picture.classList.remove("active");
+
       if (picture.className === 'card-mockup main') {
-        makeMockupVisible.style.visibility = 'hidden'; }
+        makeMockupVisible.style.visibility = 'hidden';
+
+      }
       else if (picture.className === 'card-mockup square')
         { adjustMockupByShape.className = 'card-trip-user-square';
         makeMockupVisible.style.visibility = 'visible';
+        picture.classList.add("active");
+
         }
       else if (picture.className === 'card-mockup portrait')
         { adjustMockupByShape.className = 'card-trip-user';
         makeMockupVisible.style.visibility = 'visible';
+        picture.classList.add("active");
         }
       else if (picture.className === 'card-mockup circle')
         { adjustMockupByShape.className = 'card-trip-user-circle';
         makeMockupVisible.style.visibility = 'visible';
+        picture.classList.add("active");
       }
-
-
+      console.log(picture.classList);
+      // picture.classList.remove("active");
+      // console.log(picture.classList);
     });
   })
 }
 
+
+//   mockupPictures.addEventListener("click", (e) => {
+//     mockupPictures.classList.remove("active");
+//     e.currentTarget.classList.add("active");
+//     console.log(e.currentTarget);
+//   })
+// }
 export { changeToMockupPicture };
