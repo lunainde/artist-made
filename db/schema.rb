c947205b33de42a970cb7ca15a33720a6fb752d9
Awa Said <awa.h.said@gmail.com>
2021-06-01 19:24:09 +0200

    seed file created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_201106) do
+ActiveRecord::Schema.define(version: 2021_05_29_224637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2021_05_22_201106) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "img_url"
+    t.string "artist"
+    t.string "art_theme"
+    t.string "art_format"
     t.index ["artist_id"], name: "index_arts_on_artist_id"
   end
 
